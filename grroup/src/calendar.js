66805a18aa93158e668bbd0ae66d2b94af6100f6
XifeiNi <cecilia990@outlook.com>
2019-04-28 06:07:42 +1000

    debugging...

--- grroup/src/calendar.js
+++ grroup/src/calendar.js
@@ -13,7 +13,44 @@ var setName = function(name) {
 };
 
 var addTask = function(task) {
-	
+	var list = document.createElement('li');
+	list.className = 'bdB peers ai-c jc-sb fxw-nw';
+	var a = document.createElement('a');
+	a.className = 'td-n p-20 peers fxw-nw mR-20 peer-greed c-grey-900';
+	a.href = 'javascript:void(0);';
+	// data toggle?
+
+
+	var div = document.createElement("div");
+	div.className = "peer mR-15";
+	var li = document.createElement("li");
+	li.className = "fa fa-fw fa-clock-o c-red-500";
+	div.appendChild(li);
+
+	var secondDiv = document.createElement("div");
+	secondDiv.className = "peer";
+	var span = document.createElement("span");
+	span.className = "fw-600";
+	span.innerHTML = task.name;
+
+	var thirdDiv = document.createElement("div");
+	thirdDiv.className = "c-grey-600";
+	var spanInsideThirdDiv = document.createElement("span");
+	spanInsideThirdDiv.className = "c-grey-700";
+	thirdDiv.appendChild(spanInsideThirdDiv);
+
+	secondDiv.appendChild(span);
+	secondDiv.appendChild(thirdDiv);
+
+	var i = document.createElement("i");
+	i.innerHTML = task.description;
+	thirdDiv.appendChild(i);
+
+	list.appendChild(a);
+
+	document.getElementById("task-list").appendChild(list);
+
+
 };
 
 
@@ -34,9 +71,13 @@ fetch('http://127.0.0.1:5000/api/list/users')
     })
     .then((out) => {
     	setName(out[0].name);
-    	fetchTask(out[0].assigned_tasks[0]);
+    	//fetchTask(out[0].assigned_tasks[0]);
+
+    	var text = '{"id": 1, "name": "Complete Grrroup", "description": "Let us get this Hackathon done!", "assignee": 1, "status": "exists", "color": "0000ff"}';
+    	var obj = JSON.parse(text);
+    	addTask(obj);
         console.log('Output: ', out[0].assigned_tasks);
-}).catch(err => console.error("ERROR"));
+}).catch(err => console.error(err));
 
 
 //fetch('http://web.cse.unsw.edu.au/~z5205060/grrroup/api/get/users/1')
